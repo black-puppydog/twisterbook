abd8b2bbd28f650b2c23de35ade4629d917007bf
Daan Wynen <black.puppydog@gmx.de>
2014-10-29 16:42:04 +0100

    user more threads in twisterd. not sure if this is a good idea, but it seems okay for now.

--- twisterWithMonitor.sh
+++ twisterWithMonitor.sh
@@ -2,5 +2,5 @@
 
 
 # runs the twister daemon in my setup (i.e. ../twister-core/twisterd) with the right callback to keep the blockchain current in the database
-../twister-core/twisterd -datadir=$HOME/.twister_2 -rpcuser=user -rpcpassword=pwd -rpcallowip=127.0.0.1 -rpcport=28332 -blocknotify="./BlockChainMonitor.py --only-block %s"
+../twister-core/twisterd -datadir=$HOME/.twister_2 -rpcthreads=6 -rpcuser=user -rpcpassword=pwd -rpcallowip=127.0.0.1 -rpcport=28332 -blocknotify="./BlockChainMonitor.py --only-block %s"
 
