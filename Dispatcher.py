d06950420146e0c5ec78729005b82c063007c285
Daan Wynen <black.puppydog@gmx.de>
2014-10-31 09:02:33 +0100

    adjust dispatcher query so that it mixes refreshing active users and re-trying users for who we have no profile yet.

--- Dispatcher.py
+++ Dispatcher.py
@@ -12,10 +12,19 @@ __author__ = 'daan'
 
 class Dispatcher:
     # todo: change the cache timeout to something more realistic?
-    def __init__(self, cache_timeout=3600*24):
+    def __init__(self, cache_timeout=3600 * 24, rescrape_timeout=3600 * 21):
+        """
+
+        :param cache_timeout: how long to wait until we try to pull new posts from soneone we already have posts of
+        :param rescrape_timeout: how long to wait until considering to scrape a profile again for which we failed to
+        retrieve anything before. the 21h default aims at considering each profile at different times of day so that
+        we don't miss someone just because we always try to pull their profile while they (and all their peers) are
+        at work and not runnning twister. Not sure of that's actually an issue but why not...
+        """
+        self.rescrape_timeout = rescrape_timeout
         self.log = logging.getLogger()
 
-        self.cache_timeout =cache_timeout
+        self.cache_timeout = cache_timeout
 
         self.log.debug("Connect to database...")
         self.conn = pymysql.connect(
@@ -25,17 +34,23 @@ class Dispatcher:
             db=MYSQL_DATABASE)
 
     def get_due_users(self):
-
         now = datetime.now().timestamp()
 
         cursor = self.conn.cursor()
         # todo: filter this down to due tasks in SQL if possible
-        result = cursor.execute("""SELECT username, id, last_indexed_k, last_indexed_time
+        result = cursor.execute("""(SELECT username, id, last_indexed_k, last_indexed_time
                                     FROM users as u
                                     WHERE unix_timestamp(u.last_indexed_time) + %s <= %s
                                     ORDER BY u.last_indexed_time ASC, RAND()
-                                    LIMIT 1000
-                                    """, (self.cache_timeout, now))
+                                    LIMIT 1000)
+                                    UNION
+                                    (SELECT username, id, last_indexed_k, last_indexed_time
+                                    FROM users AS u
+                                    WHERE unix_timestamp(u.last_indexed_time) >100 -- we tried this user before
+                                      AND u.last_indexed_k = -1
+                                      AND unix_timestamp(u.last_indexed_time) + %s <= %s
+                                    LIMIT 2000);
+                                    """, (self.cache_timeout, now, self.rescrape_timeout, now))
         print("User that need scraping: %i" % result)
 
         # todo: if filtering in SQL works then this should be unneccessary
