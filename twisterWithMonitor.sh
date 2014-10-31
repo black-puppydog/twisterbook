d514f213315a5b95aca8970ce7e01ab6602a8cd7
Daan Wynen <black.puppydog@gmx.de>
2014-10-31 14:02:28 +0100

    make twister wrapper script write logs to terminal. it is going to run ins creen anyways :P

--- twisterWithMonitor.sh
+++ twisterWithMonitor.sh
@@ -2,5 +2,5 @@
 
 
 # runs the twister daemon in my setup (i.e. ../twister-core/twisterd) with the right callback to keep the blockchain current in the database
-../twister-core/twisterd -datadir=$HOME/.twister -rpcthreads=6 -rpcuser=user -rpcpassword=pwd -rpcallowip=127.0.0.1 -rpcport=28332 -blocknotify="./BlockChainMonitor.py --only-block %s"
+../twister-core/twisterd -datadir=$HOME/.twister -rpcthreads=6 -rpcuser=user -rpcpassword=pwd -rpcallowip=127.0.0.1 -rpcport=28332 -printtoconsole -blocknotify="./BlockChainMonitor.py --only-block %s"
 
